e916545696721d93b41a74a844d9449e513793a8
Davide Fissore <fisso9900@gmail.com>
2023-07-05 19:22:53 +0200

    Create misc.tsx

---
+++ src/components/elpi/misc.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Int() {
+  return (<React.Fragment>!</React.Fragment>)
+}
+
+export function Discard() {
+  return (<React.Fragment>_</React.Fragment>)
+}
