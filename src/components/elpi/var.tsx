dfcee9515c919eab8e4c727fcfe4428adb3791ef
Davide Fissore <fisso9900@gmail.com>
2023-07-15 14:16:35 +0200

    remove useless files

--- src/components/elpi/var.tsx
+++
@@ -1,9 +0,0 @@
-import { VarType } from '../types';
-
-
-function Var({ name }: VarType) {
-  let [name1, index] = name.split(" ");
-  return (<span className='var'>{name1}<sub>{index}</sub></span>)
-}
-
-export default Var;
